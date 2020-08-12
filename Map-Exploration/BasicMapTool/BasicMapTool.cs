adf1666292446169fc1e01b3a15f37088a29f091
NavigateByCloud <swingliu0831@foxmail.com>
2020-08-12 15:26:34 -0400

    test camera look at

--- Map-Exploration/BasicMapTool/BasicMapTool.cs
+++ Map-Exploration/BasicMapTool/BasicMapTool.cs
@@ -23,34 +23,130 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using ArcGIS.Core.CIM;
+using ArcGIS.Desktop.Framework;
 using ArcGIS.Desktop.Framework.Threading.Tasks;
 using ArcGIS.Desktop.Mapping;
+using ArcGIS.Desktop.Mapping.Events;
 
 namespace BasicMapTool {
     internal class BasicMapTool : MapTool {
 
         private bool _zMode = false;
+        Camera mainCamera;
+        //double x, y, z;
 
         public BasicMapTool() : base() {
+            x = y = z = 0;
             this.OverlayControlID = "BasicMapTool_BasicEmbeddableControl";
             //Embeddable control can be resized
             OverlayControlCanResize = true;
             //Specify ratio of 0 to 1 to place the control
             OverlayControlPositionRatio = new System.Windows.Point(0, 0); //top left
+            _zoomToCmd = new RelayCommand(() => MapView.Active.ZoomToAsync(Camera, TimeSpan.FromSeconds(1.5)), () => { return MapView.Active != null; });
+            _panToCmd = new RelayCommand(() => MapView.Active.PanToAsync(Camera, TimeSpan.FromSeconds(1.5)), () => { return MapView.Active != null; });
+
+            MapViewCameraChangedEvent.Subscribe(OnCameraChanged);
+            ActiveMapViewChangedEvent.Subscribe(OnActiveMapViewChanged);
+
+            if (MapView.Active != null)
+                Camera = MapView.Active.Camera; x = Camera.X; y = Camera.Y; z = Camera.Z;
         }
+
+        ~BasicMapTool()
+        {
+            _zoomToCmd.Disconnect();
+            _panToCmd.Disconnect();
+
+            ActiveMapViewChangedEvent.Unsubscribe(OnActiveMapViewChanged);
+            MapViewCameraChangedEvent.Unsubscribe(OnCameraChanged);
+        }
+
+        private Camera _camera;
+        private double x, y, z;
+        public Camera Camera {
+            get { return _camera; }
+            set {
+                SetProperty(ref _camera, value, () => Camera);
+            }
+        }
+
+        private RelayCommand _panToCmd;
+        public ICommand PanToCmd {
+            get { return _panToCmd; }
+        }
+
+        private RelayCommand _zoomToCmd;
+        public ICommand ZoomToCmd {
+            get { return _zoomToCmd; }
+        }
+
         protected override Task OnToolActivateAsync(bool active) {
             return base.OnToolActivateAsync(active);
         }
 
+        private void OnCameraChanged(MapViewCameraChangedEventArgs obj)
+        {
+            if (obj.MapView == MapView.Active)
+                Camera = obj.CurrentCamera;
+            updateText(Camera);
+        }
+
+        private void OnActiveMapViewChanged(ActiveMapViewChangedEventArgs obj)
+        {
+            if (obj.IncomingView == null)
+            {
+                Camera = null;
+                return;
+            }
+
+            Camera = obj.IncomingView.Camera;
+            updateText(Camera);
+        }
+
+
         protected override void OnToolKeyDown(MapViewKeyEventArgs k) {
             //We will do some basic key handling to allow panning
             //from the arrow keys
-            if (k.Key == Key.Left ||
-                k.Key == Key.Right ||
-                k.Key == Key.Up ||
-                k.Key == Key.Down)
+            if (k.Key == Key.Left || // -x 
+                k.Key == Key.Right || // x
+                k.Key == Key.Up || // y
+                k.Key == Key.Down || // -y
+                k.Key == Key.W || // z
+                k.Key == Key.S || // -z
+                k.Key == Key.A || // heading
+                k.Key == Key.D || // -heading
+                k.Key == Key.Q || // scale
+                k.Key == Key.E || // -scale
+                k.Key == Key.Z || // roll
+                k.Key == Key.X || // -roll
+                k.Key == Key.C || // pitch
+                k.Key == Key.V) // -pitch
                 k.Handled = true;
             base.OnToolKeyDown(k);
+        }
+
+        void updateText(Camera camera)
+        {
+            var vm = OverlayEmbeddableControl as BasicEmbeddableControlViewModel;
+            if (vm == null)
+                return;
+
+            var sb = new StringBuilder();
+            sb.AppendLine(string.Format("X: {0}", camera.X));
+            sb.Append(string.Format("Y: {0}", camera.Y));
+            sb.AppendLine();
+            sb.Append(string.Format("Z: {0}", camera.Z));
+            sb.AppendLine();
+            sb.Append(string.Format("heading: {0}", camera.Heading));
+            sb.AppendLine();
+            sb.Append(string.Format("pitch: {0}", camera.Pitch));
+            sb.AppendLine();
+            sb.Append(string.Format("roll: {0}", camera.Roll));
+            sb.AppendLine();
+            sb.Append(string.Format("scale: {0}", camera.Scale));
+            sb.AppendLine();
+            sb.Append(camera.Viewpoint.ToString());
+            vm.Text = sb.ToString();
         }
 
         protected override Task HandleKeyDownAsync(MapViewKeyEventArgs k) {
@@ -63,11 +159,14 @@ namespace BasicMapTool {
             bool shiftKey = (Keyboard.Modifiers & ModifierKeys.Shift)
                                      == ModifierKeys.Shift;
 
+            float degree = 10f;
             //When in 3D mode use the Shift key to switch from "Y" position change
             //which is 'Up','Down' in 2D but is 'forward','back' in 3D to
             //"Z" position change which is 'Up','Down' in 3D
             if (!_zMode && ActiveMapView.ViewingMode != MapViewingMode.Map)
+            {
                 _zMode = shiftKey;
+            }
 
             switch (k.Key) {
                 case Key.Left:
@@ -81,7 +180,7 @@ namespace BasicMapTool {
                         camera.Z += dz;
                     }
                     else {
-                        camera.Y += dy;
+                       camera.Y += dy;
                     }
                     break;
                 case Key.Down:
@@ -92,7 +191,62 @@ namespace BasicMapTool {
                         camera.Y -= dy;
                     }
                     break;
+                case Key.Q: 
+                    if (_zMode) {
+                        //camera.Viewpoint = CameraViewpoint.LookFrom;
+                        
+                        camera.Heading += 10;
+                    }
+                    else {
+                        camera.Viewpoint = CameraViewpoint.LookAt;
+                        camera.X = x;
+                        camera.Y = y;
+                        camera.Z = z;
+                        camera.Heading += 10;
+                    }
+                    break;
+                case Key.E:
+                    if (_zMode)
+                    {
+                        //camera.Viewpoint = CameraViewpoint.LookFrom;
+
+                        camera.Heading -= 10;
+                    }
+                    else
+                    {
+                        camera.Viewpoint = CameraViewpoint.LookAt;
+                        camera.X = x;
+                        camera.Y = y;
+                        camera.Z = z;
+
+                        camera.Heading -= 10;
+                    }
+                    break;
+                case Key.W:
+                    if (_zMode)
+                    {
+                        camera.Scale -= 100;
+                    }
+                    else
+                    {
+                        camera.Scale -= 100;
+                    }
+                    break;
+                case Key.S:
+                    if (_zMode)
+                    {
+                        camera.Scale += 100;
+                    }
+                    else
+                    {
+                        camera.Scale += 100;
+                    }
+                    break;
+                case Key.A: break;
+                case Key.D: break;
+
             }
+            
             return MapView.Active.ZoomToAsync(camera, new TimeSpan(0, 0, 0, 0, 250));
         }
 
@@ -104,6 +258,11 @@ namespace BasicMapTool {
             //On mouse down check if the mouse button pressed is the left mouse button. If it is handle the event.
             if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
                 e.Handled = true;
+        }
+        Camera CopyCamera(Camera copyFrom)
+        {
+
+            return new Camera(copyFrom.X, copyFrom.Y, copyFrom.Z, copyFrom.Scale, copyFrom.Heading, copyFrom.SpatialReference, copyFrom.Viewpoint);
         }
 
         protected override Task HandleMouseDownAsync(MapViewMouseButtonEventArgs e) {
